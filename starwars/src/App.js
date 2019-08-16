8b4edb53a69366f23bf53a670d6cc9ed86dc2f8f
allisonkydy <allisnelly@gmail.com>
2019-08-16 12:23:53 -0500

    add pagination fn and styled buttons

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -13,14 +13,23 @@ const App = () => {
   // sync up with, if any.
 
   const [peopleData, setPeopleData] = useState();
+  const [page, setPage] = useState(1);
+
+  function getCharacters(direction) {
+    if (direction === "next") {
+      page === 9 ? setPage(1) : setPage(page => page + 1);
+    } else if (direction === "previous") {
+      page === 1 ? setPage(9) : setPage(page => page - 1);
+    }
+  }
 
   useEffect(() => {
-    axios.get('https://swapi.co/api/people/')
+    axios.get(`https://swapi.co/api/people/?page=${page}`)
       .then(response => {
         setPeopleData(response.data);
       })
       .catch(err => console.log(err))
-  }, [])
+  }, [page])
 
   if (!peopleData) return <h3>Fetching data...</h3>
 
@@ -32,9 +41,27 @@ const App = () => {
     align-content: flex-start;
   `
 
+  const Button = styled.button`
+    min-width: 100px;
+    border: 2px solid black;
+    background: transparent;
+    padding: 10px;
+    border-radius: 5px;
+    margin: 0 20px 30px;
+    font-weight: bold;
+    font-size: 1.6rem;
+
+    &:hover {
+      cursor: pointer;
+      background: white;
+    }
+  `
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Button onClick={() => getCharacters("previous")}>Previous</Button>
+      <Button onClick={() => getCharacters("next")}>Next</Button>
       <PersonContainer>
         {peopleData.results.map(person => {
         return <Person person={person} key={person.name}/>
